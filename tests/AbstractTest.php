885493c2b0d67018970561199b08f615a5929d9f
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-21 08:46:03 +0100

    move stuff to traits

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -1,45 +1,20 @@
 <?php
 
+require 'SelectTest.php';
+require 'InsertTest.php';
+require 'UpdateTest.php';
+require 'DeleteTest.php';
+
 abstract class AbstractTest extends PHPUnit_Extensions_Database_TestCase
 {
+    use SelectTest;
+    use InsertTest;
+    use UpdateTest;
+    use DeleteTest;
+
     public function getDataSet()
     {
         return $this->createXMLDataSet(dirname(__FILE__).'/_files/data.xml');
-    }
-
-    public function testSelects()
-    {
-        $db = $this->getConnection()->getConnection();
-        $result = $db->select('test', '*');
-        $test = [];
-        foreach ($result() as $row) {
-            $test[] = (int)$row['id'];
-        }
-        $this->assertEquals([1, 2, 3], $test);
-
-        // Re-query should also work, yielding a new result set:
-        $result = $db->select('test', '*', [], ['order' => 'id']);
-        $test = [];
-        foreach ($result() as $row) {
-            $test[] = (int)$row['id'];
-        }
-        $this->assertEquals([1, 2, 3], $test);
-
-        $db = $this->getConnection()->getConnection();
-        $result = $db->fetch('test', '*', [], ['order' => 'id']);
-        $this->assertEquals(1, (int)$result['id']);
-
-        $result = $db->column('test', 'id', [], ['order' => 'id']);
-        $this->assertEquals(1, (int)$result);
-    }
-
-    /**
-     * @expectedException Dabble\Query\SelectException
-     */
-    public function testNoResults()
-    {
-        $db = $this->getConnection()->getConnection();
-        $db->select('test', '*', ['id' => 12345]);
     }
 }
 
