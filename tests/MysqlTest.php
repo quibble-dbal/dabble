afd896f4f15ebcc432d649cc45e33dec8fc32e8b
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-21 07:55:25 +0100

    move to generic base class; the whole idea is we can write queries in a database-agnostic manner, after all

--- tests/MysqlTest.php
+++ tests/MysqlTest.php
@@ -2,7 +2,7 @@
 
 use Dabble\Adapter\Mysql;
 
-class MysqlTest extends PHPUnit_Extensions_Database_TestCase
+class MysqlTest extends AbstractTest
 {
     public function getConnection()
     {
@@ -18,40 +18,6 @@ class MysqlTest extends PHPUnit_Extensions_Database_TestCase
             );
         }
         return $db;
-    }
-
-    public function getDataSet()
-    {
-        return $this->createXMLDataSet(dirname(__FILE__).'/_files/data.xml');
-    }
-
-    public function testAllRows()
-    {
-        $db = $this->getConnection()->getConnection();
-        $result = $db->select('test', '*');
-        $test = [];
-        foreach ($result() as $row) {
-            $test[] = (int)$row['id'];
-        }
-        $this->assertEquals([1, 2, 3], $test);
-
-        // Re-query should also work, yielding a new result set:
-        $result = $db->select('test', '*');
-        $test = [];
-        foreach ($result() as $row) {
-            $test[] = (int)$row['id'];
-        }
-        $this->assertEquals([1, 2, 3], $test);
-    }
-
-    public function testOtherSelectMethods()
-    {
-        $db = $this->getConnection()->getConnection();
-        $result = $db->fetch('test', '*');
-        $this->assertEquals(1, (int)$result['id']);
-
-        $result = $db->column('test', 'id');
-        $this->assertEquals(1, (int)$result);
     }
 }
 
