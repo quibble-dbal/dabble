4d557d096e71fd784269c51b309adaa5308c49aa
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-21 07:55:54 +0100

    sqlite test setup

---
+++ tests/SqliteTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Dabble\Adapter\Sqlite;
+
+class SqliteTest extends AbstractTest
+{
+    public function getConnection()
+    {
+        static $db;
+        if (!isset($db)) {
+            $db = $this->createDefaultDBConnection(
+                new Sqlite(':memory:'),
+                'dabble_test'
+            );
+            $schema = file_get_contents(
+                dirname(__FILE__).'/_files/schema.sqlite.sql'
+            );
+            $db->getConnection()->exec($schema);
+        }
+        return $db;
+    }
+}
+
