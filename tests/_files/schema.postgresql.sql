d1ddccc41404a4e31b6347db31610f869274ea4f
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-13 15:41:40 +0200

    update schemas

--- tests/_files/schema.postgresql.sql
+++ tests/_files/schema.postgresql.sql
@@ -9,3 +9,10 @@ CREATE UNIQUE INDEX test_name_key ON test(name);
 CREATE INDEX test_status_key ON test(status);
 CREATE INDEX test_datecreated_key ON test(datecreated);
 
+CREATE TABLE test2 (
+    id SERIAL PRIMARY KEY,
+    test INTEGER NOT NULL REFERENCES test(id) ON DELETE CASCADE,
+    data VARCHAR(255)
+);
+CREATE INDEX test2_test_key ON test2(test);
+
