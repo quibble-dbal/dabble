36cc9b2de3806eafd1c000de3f398c1d6bb575cd
Marijn Ophorst <marijn@sensimedia.nl>
2016-01-23 13:26:26 +0100

    upgrade

--- tests/InsertTest.php
+++ tests/InsertTest.php
@@ -5,21 +5,26 @@ namespace Dabble\Test;
 use Dabble\Adapter;
 use Dabble\Query\InsertException;
 
+/**
+ * @Feature Insertions
+ */
 trait InsertTest
 {
     /**
-     * @Description {0}::insert should insert a new row
+     * @Scenario {0}::insert should insert a new row
      */
-    public function testInsert(Adapter $db, $table = 'test', $values = ['name' => 'monomelodies'])
+    public function testInsert(Adapter &$db = null, $table = 'test', $values = ['name' => 'monomelodies'])
     {
+        $db = $this->db;
         return 1;
     }
 
     /**
-     * @Description {0}::insert should throw an exception if nothing was inserted
+     * @Scenario {0}::insert should throw an exception if nothing was inserted
      */
-    public function testNoInsert(Adapter $db, $table = 'test', $values = ['name' => null])
+    public function testNoInsert(Adapter &$db = null, $table = 'test', $values = ['name' => null])
     {
+        $db = $this->db;
         throw new InsertException;
     }
 }
