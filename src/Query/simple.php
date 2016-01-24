5672996704b9a7f5dc22dfec4f7bbc468e42dd92
Marijn Ophorst <marijn@sensimedia.nl>
2016-01-24 11:06:32 +0100

    normalize results

--- src/Query/simple.php
+++ src/Query/simple.php
@@ -3,6 +3,7 @@
 return function () use ($stmt, &$first) {
     $ret = [$first];
     $ret = array_merge($ret, $stmt->fetchAll(PDO::FETCH_ASSOC));
+    array_walk($ret, [$this, 'normalize']);
     return $ret;
 };
 
