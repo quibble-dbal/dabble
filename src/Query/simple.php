0baceb400989c6a4f2f388b4b2c97597a31e6547
Marijn Ophorst <marijn@sensimedia.nl>
2016-02-01 10:51:01 +0100

    return an array

--- src/Query/simple.php
+++ src/Query/simple.php
@@ -1,9 +1,7 @@
 <?php
 
-return function () use ($stmt, &$first) {
-    $ret = [$first];
-    $ret = array_merge($ret, $stmt->fetchAll(PDO::FETCH_ASSOC));
-    array_walk($ret, [$this, 'normalize']);
-    return $ret;
-};
+$ret = [$first];
+$ret = array_merge($ret, $stmt->fetchAll(PDO::FETCH_ASSOC));
+array_walk($ret, [$this, 'normalize']);
+return $ret;
 
