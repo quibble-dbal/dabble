ec8da234ffcb5c9a1b280d5ffc5602e72087848c
Marijn Ophorst <marijn@sensimedia.nl>
2016-08-28 11:32:41 +0200

    add base exception

---
+++ src/Exception.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Base Exception that other Dabble-exceptions should extend.
+ *
+ * @package Quibble\Dabble
+ * @author Marijn Ophorst <marijn@monomelodies.nl>
+ * @copyright MonoMelodies 2010, 2015, 2016
+ */
+
+namespace Quibble\Dabble;
+
+/**
+ * This is just a stub and shouldn't get thrown.
+ */
+abstract class Exception extends \Exception
+{
+}
+
