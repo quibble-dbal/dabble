be3eea6f615ce4c187eeefa7cf7ea7649ad1edcb
Marijn Ophorst <marijn@sensimedia.nl>
2016-09-11 14:14:19 +0200

    this must satisfy the contract

--- tests/setup.php
+++ tests/setup.php
@@ -7,5 +7,23 @@ use Quibble\Dabble;
 global $adapter;
 
 $adapter = new class('sqlite::memory:') extends Dabble\Adapter {
+    public function now() : Dabble\Raw
+    {
+        return new Dabble\Raw('CURRENT_TIMESTAMP');
+    }
+
+    public function random() : Dabble\Raw
+    {
+        return new Dabble\Raw('RANDOM()');
+    }
+
+    public function interval($offset, $unit, $amount) : Dabble\Raw
+    {
+        if ($offset instanceof Dabble\Now) {
+            $offset = "'now'";
+        }
+        return new Dabble\Raw("datetime($offset, '$unit {$amount}s");
+    }
+
 };
 
