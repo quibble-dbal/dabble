eebbf2b10648ade73d60fb1dca23cef58dd0964d
Marijn Ophorst <marijn@sensimedia.nl>
2018-07-19 13:03:16 +0200

    fix test setup

--- tests/setup.php
+++ tests/setup.php
@@ -7,19 +7,19 @@ use Quibble\Dabble;
 global $adapter;
 
 $adapter = new class('sqlite::memory:') extends Dabble\Adapter {
-    public function now() : Dabble\Raw
+    public function now() : string
     {
-        return new Dabble\Raw('CURRENT_TIMESTAMP');
+        return 'CURRENT_TIMESTAMP';
     }
 
-    public function random() : Dabble\Raw
+    public function random() : string
     {
-        return new Dabble\Raw('RANDOM()');
+        return 'RANDOM()';
     }
 
-    public function interval($unit, $amount) : Dabble\Raw
+    public function interval($unit, $amount) : string
     {
-        return new Dabble\Raw("datetime('now', '$unit {$amount}s");
+        return "datetime('now', '$unit {$amount}s";
     }
 
 };
