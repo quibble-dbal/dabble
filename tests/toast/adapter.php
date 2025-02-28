bbe62f204c073f91ae450fe715face7d2156f066
Marijn Ophorst <marijn@sensimedia.nl>
2025-02-28 13:52:02 +0100

    test slightly better

--- tests/toast/adapter.php
+++ tests/toast/adapter.php
@@ -4,10 +4,11 @@ require __DIR__.'/../setup.php';
 
 /** Tests for adapter */
 return function () use ($adapter) : Generator {
-    /**When creating a new adapter, it is an instanceof PDO */
+    /** When creating a new adapter, it is an instanceof PDO */
     yield function () use ($adapter) {
         global $adapter;
         assert($adapter instanceof PDO);
+        assert($adapter instanceof Quibble\Dabble\Adapter);
     };
 };
 
